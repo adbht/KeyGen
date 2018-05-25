a9f03ff3d3ed957843fcc9ed913c7c39ad78a316
Aditya Bhatia <30832959+adbht@users.noreply.github.com>
2018-05-25 12:48:01 -0400

    Add files via upload

---
+++ KeyGenUITests/KeyGenUITests.swift
@@ -0,0 +1,22 @@
+//  KeyGenUITests.swift
+//  KeyGenUITests
+//  Created by Adi Bhatia on 2018-05-25.
+//  Copyright © 2018 adbht. All rights reserved.
+
+import XCTest
+
+class KeyGenUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+    }
+}
