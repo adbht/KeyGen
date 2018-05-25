822d27d8fbbb710f1602a929627801a8beb2544a
Aditya Bhatia <30832959+adbht@users.noreply.github.com>
2018-05-25 12:46:12 -0400

    Add files via upload

---
+++ KeyGenTests/KeyGenTests.swift
@@ -0,0 +1,26 @@
+//  KeyGenTests.swift
+//  KeyGenTests
+//  Created by Adi Bhatia on 2018-05-25.
+//  Copyright © 2018 adbht. All rights reserved.
+
+import XCTest
+@testable import KeyGen
+
+class KeyGenTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+    }
+    
+    func testPerformanceExample() {
+        self.measure {
+        }
+    }
+}
