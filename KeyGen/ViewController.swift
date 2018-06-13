8fa3af7678d2139348d46188fcb70ec64dc42d3a
Aditya Bhatia <30832959+adbht@users.noreply.github.com>
2018-06-13 10:00:55 -0400

    Update ViewController.swift

--- KeyGen/ViewController.swift
+++ KeyGen/ViewController.swift
@@ -4,10 +4,11 @@
 //  Copyright © 2018 adbht. All rights reserved.
 
 import UIKit
-import Foundation
+import Foundation //This is needed to round off the the slider value and display a whole number using round()
 
 class ViewController: UIViewController {
-
+    
+    //Connecting all the outlets and actions needed from Main.storyboard
     @IBOutlet weak var KeyNumber: UILabel!
     @IBOutlet weak var SliderOutlet: UISlider!
     @IBOutlet weak var LettersSwitch: UISwitch!
@@ -17,13 +18,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var CopyOutlet: UIButton!
     
     @IBAction func SliderAction(_ sender: UISlider) {
-        KeyNumber.text = "\(round(SliderOutlet.value))"
+        KeyNumber.text = "\(round(SliderOutlet.value))" //This will change the value of KeyNumber outlet to the slider value.
     }
+    
     @IBAction func Generate(_ sender: Any) {
-        var newKey : String = ""
-        if LettersSwitch.isOn == false && DigitsSwitch.isOn == false && SymbolsSwitch.isOn == false { // Error check to see if user has switched off all 3 switches
+        var newKey : String = "" //Defining the new password. This will be changed to an actual password after checking user configuration.
+  
+        if LettersSwitch.isOn == false && DigitsSwitch.isOn == false && SymbolsSwitch.isOn == false { //Error check to see if user has switched off all 3 switches
             Password.text = "Password cannot be generated without letters, digits, and symbols"
-        } else if LettersSwitch.isOn == true && DigitsSwitch.isOn == false && SymbolsSwitch.isOn == false { // If user has only switched on letters
+        } else if LettersSwitch.isOn == true && DigitsSwitch.isOn == false && SymbolsSwitch.isOn == false { //If user has only switched on letters
             let characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
             for i in 1...Int(SliderOutlet.value) {
                 let randomIndex = Int(arc4random_uniform(UInt32(characters.count)))
@@ -34,7 +37,7 @@ class ViewController: UIViewController {
                 }
             }
             Password.text = newKey
-            CopyOutlet.isHidden = false
+            CopyOutlet.isHidden = false //Giving user the option to copy their new password now that it has been generated.
         } else if LettersSwitch.isOn == false && DigitsSwitch.isOn == true && SymbolsSwitch.isOn == false { // If user has only switched on Digits
             let characters = ["0","1","2","3","4","5","6","7","8","9"]
             for i in 1...Int(SliderOutlet.value) {
@@ -46,7 +49,7 @@ class ViewController: UIViewController {
                 }
             }
             Password.text = newKey
-            CopyOutlet.isHidden = false
+            CopyOutlet.isHidden = false //Giving user the option to copy their new password now that it has been generated.
         } else if LettersSwitch.isOn == false && DigitsSwitch.isOn == false && SymbolsSwitch.isOn == true { // If user has only switched on Symbols
             let characters = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}","\\","|",";",":","'","\"",",","<",".",">","/","?"]
             for i in 1...Int(SliderOutlet.value) {
@@ -58,7 +61,7 @@ class ViewController: UIViewController {
                 }
             }
             Password.text = newKey
-            CopyOutlet.isHidden = false
+            CopyOutlet.isHidden = false //Giving user the option to copy their new password now that it has been generated.
         } else if LettersSwitch.isOn == true && DigitsSwitch.isOn == true && SymbolsSwitch.isOn == false { // If user has switched on Letters and Digits
             let characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"]
             for i in 1...Int(SliderOutlet.value) {
@@ -70,7 +73,7 @@ class ViewController: UIViewController {
                 }
             }
             Password.text = newKey
-            CopyOutlet.isHidden = false
+            CopyOutlet.isHidden = false //Giving user the option to copy their new password now that it has been generated.
         } else if LettersSwitch.isOn == true && DigitsSwitch.isOn == false && SymbolsSwitch.isOn == true { // If user has switched on Letters and Symbols
             let characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}","\\","|",";",":","'","\"",",","<",".",">","/","?"]
             for i in 1...Int(SliderOutlet.value) {
@@ -82,7 +85,7 @@ class ViewController: UIViewController {
                 }
             }
             Password.text = newKey
-            CopyOutlet.isHidden = false
+            CopyOutlet.isHidden = false //Giving user the option to copy their new password now that it has been generated.
         } else if LettersSwitch.isOn == false && DigitsSwitch.isOn == true && SymbolsSwitch.isOn == true { // If user has switched on Digits and Symbols
             let characters = ["0","1","2","3","4","5","6","7","8","9","`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}","\\","|",";",":","'","\"",",","<",".",">","/","?"]
             
@@ -95,7 +98,7 @@ class ViewController: UIViewController {
                 }
             }
             Password.text = newKey
-            CopyOutlet.isHidden = false
+            CopyOutlet.isHidden = false //Giving user the option to copy their new password now that it has been generated.
         } else { // If user has switched on all three switches
             let characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9","`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}","\\","|",";",":","'","\"",",","<",".",">","/","?"]
             
@@ -108,10 +111,10 @@ class ViewController: UIViewController {
                 }
             }
             Password.text = newKey
-            CopyOutlet.isHidden = false
+            CopyOutlet.isHidden = false //Giving user the option to copy their new password now that it has been generated.
         }
     }
-    @IBAction func CopyAction(_ sender: Any) {
+    @IBAction func CopyAction(_ sender: Any) { //To copy the new password to the clipboard.
         let pasteboard = UIPasteboard.general
         pasteboard.string = Password.text
     }
