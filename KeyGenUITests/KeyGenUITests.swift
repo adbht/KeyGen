011142d04b0715b8b00f0a9eaec355e36fe4d1e2
Aditya Bhatia <30832959+adbht@users.noreply.github.com>
2018-06-15 12:44:37 -0400

    Update KeyGenUITests.swift

--- KeyGenUITests/KeyGenUITests.swift
+++ KeyGenUITests/KeyGenUITests.swift
@@ -6,17 +6,14 @@
 import XCTest
 
 class KeyGenUITests: XCTestCase {
-        
     override func setUp() {
         super.setUp()
         continueAfterFailure = false
         XCUIApplication().launch()
     }
-    
     override func tearDown() {
         super.tearDown()
     }
-    
     func testExample() {
     }
 }
