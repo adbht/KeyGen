b201e44d1fbe88e2248580ecf9ffe3cb42b9be47
Aditya Bhatia <adbht@outlook.com>
2018-09-30 18:48:00 -0400

    Used .randomElement() method to cut few lines

--- KeyGen/ViewController.swift
+++ KeyGen/ViewController.swift
@@ -23,62 +23,55 @@ class ViewController: UIViewController {
     
     //Main function to control what to do after the user has pressed the "Generate" button
     @IBAction func generate(_ sender: Any) {
-        var newKey : String = ""    //Defining the new password. This will be changed to the actual password after checking user configuration
+        var newKey = String()    //Defining the new password. This will be changed to the actual password after checking user configuration
         if lettersSwitch.isOn == false && digitsSwitch.isOn == false && symbolsSwitch.isOn == false { //Error check to see if user has switched off all 3 switches
             password.text = "Password cannot be generated without letters, digits, and symbols"
         } else if lettersSwitch.isOn == true && digitsSwitch.isOn == false && symbolsSwitch.isOn == false { //If user has only switched on letters
             let characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
             for _ in 1...Int(sliderOutlet.value) {
-                let randomIndex = Int(arc4random_uniform(UInt32(characters.count)))
-                newKey += "\(characters[randomIndex])"
+                newKey += "\(characters.randomElement()!)"
             }
-            password.text = newKey      //To replace the empty label with the new password text
-            copyOutlet.isHidden = false //Giving user the option to copy their new password now that it has been generated
+            password.text = newKey          //To replace the empty label with the new password text
+            copyOutlet.isHidden = false     //Giving user the option to copy their new password now that it has been generated
         } else if lettersSwitch.isOn == false && digitsSwitch.isOn == true && symbolsSwitch.isOn == false { //If user has only switched on Digits
             let characters = ["0","1","2","3","4","5","6","7","8","9"]
             for _ in 1...Int(sliderOutlet.value) {
-                let randomIndex = Int(arc4random_uniform(UInt32(characters.count)))
-                newKey += "\(characters[randomIndex])"
+                newKey += "\(characters.randomElement()!)"
             }
             password.text = newKey      //To replace the empty label with the new password text
             copyOutlet.isHidden = false //Giving user the option to copy their new password now that it has been generated
         } else if lettersSwitch.isOn == false && digitsSwitch.isOn == false && symbolsSwitch.isOn == true { //If user has only switched on Symbols
             let characters = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}","\\","|",";",":","'","\"",",","<",".",">","/","?"]
             for _ in 1...Int(sliderOutlet.value) {
-                let randomIndex = Int(arc4random_uniform(UInt32(characters.count)))
-                newKey += "\(characters[randomIndex])"
+                newKey += "\(characters.randomElement()!)"
             }
             password.text = newKey      //To replace the empty label with the new password text
             copyOutlet.isHidden = false //Giving user the option to copy their new password now that it has been generated
         } else if lettersSwitch.isOn == true && digitsSwitch.isOn == true && symbolsSwitch.isOn == false { //If user has switched on Letters and Digits
             let characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"]
             for _ in 1...Int(sliderOutlet.value) {
-                let randomIndex = Int(arc4random_uniform(UInt32(characters.count)))
-                newKey += "\(characters[randomIndex])"
+                newKey += "\(characters.randomElement()!)"
             }
             password.text = newKey      //To replace the empty label with the new password text
             copyOutlet.isHidden = false //Giving user the option to copy their new password now that it has been generated
         } else if lettersSwitch.isOn == true && digitsSwitch.isOn == false && symbolsSwitch.isOn == true { //If user has switched on Letters and Symbols
             let characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}","\\","|",";",":","'","\"",",","<",".",">","/","?"]
             for _ in 1...Int(sliderOutlet.value) {
-                let randomIndex = Int(arc4random_uniform(UInt32(characters.count)))
-                newKey += "\(characters[randomIndex])"
+                newKey += "\(characters.randomElement()!)"
             }
             password.text = newKey      //To replace the empty label with the new password text
             copyOutlet.isHidden = false //Giving user the option to copy their new password now that it has been generated
         } else if lettersSwitch.isOn == false && digitsSwitch.isOn == true && symbolsSwitch.isOn == true { //If user has switched on Digits and Symbols
             let characters = ["0","1","2","3","4","5","6","7","8","9","`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}","\\","|",";",":","'","\"",",","<",".",">","/","?"]
             for _ in 1...Int(sliderOutlet.value) {
-                let randomIndex = Int(arc4random_uniform(UInt32(characters.count)))
-                newKey += "\(characters[randomIndex])"
+                newKey += "\(characters.randomElement()!)"
             }
             password.text = newKey      //To replace the empty label with the new password text
             copyOutlet.isHidden = false //Giving user the option to copy their new password now that it has been generated
         } else { //Finally, the only other possibility, if user has switched on all three switches
             let characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9","`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}","\\","|",";",":","'","\"",",","<",".",">","/","?"]
             for _ in 1...Int(sliderOutlet.value) {
-                let randomIndex = Int(arc4random_uniform(UInt32(characters.count)))
-                newKey += "\(characters[randomIndex])"
+                newKey += "\(characters.randomElement()!)"
             }
             password.text = newKey      //To replace the empty label with the new password text
             copyOutlet.isHidden = false //Giving user the option to copy their new password now that it has been generated
